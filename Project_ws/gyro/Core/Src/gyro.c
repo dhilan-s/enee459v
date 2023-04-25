eb0674f8901b24aae6396ec592731aea9f45c869
dhilan-s <98917575+dhilan-s@users.noreply.github.com>
2023-04-25 16:03:52 -0400

    Skeleton code

--- Project_ws/gyro/Core/Src/gyro.c
+++ Project_ws/gyro/Core/Src/gyro.c
@@ -14,10 +14,10 @@ void what_is_it(){
 
 //function which changes content of some data or control register of the peripheral.
 void write_location(){
-
+	HAL_I2C_Mem_Write (I2C_HandleTypeDef * hi2c, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t * pData, uint16_t Size, uint32_t Timeout);
 }
 
 //function which reports contents of some data or control register of the peripheral.
 void read_location(){
-
+	HAL_I2C_Mem_Read (I2C_HandleTypeDef * hi2c, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t * pData, uint16_t Size, uint32_t Timeout);
 }
