158491835d8e7e72f7ad35f39e1759d26b19f3dc
dhilan-s <98917575+dhilan-s@users.noreply.github.com>
2023-04-24 21:50:32 -0400

    Started project file

---
+++ Project_ws/gyro/Core/Src/gyro.c
@@ -0,0 +1,23 @@
+/*
+ * gyro.c
+ *
+ *  Created on: Apr 24, 2023
+ *      Author: PC
+ */
+
+
+//function which reports some unique signature of the target peripheral (WHO_AM_I or equivalent).
+void what_is_it(){
+
+}
+
+
+//function which changes content of some data or control register of the peripheral.
+void write_location(){
+
+}
+
+//function which reports contents of some data or control register of the peripheral.
+void read_location(){
+
+}
